8dc395c0934834820b22b35eb2215ec03daea861
anticitizen4 <20382674+anticitizen4@users.noreply.github.com>
2018-11-09 11:01:19 +0300

    make use of HOC

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -2,32 +2,26 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-const MainMenu = props => {
+function bindProps(Component, propsToBind) {
+	return props => <Component {...propsToBind} {...props} />;
+}
+
+const props = {
+	as: NavLink,
+	exact: true,
+	activeClassName: "active",
+};
+
+const BoundMenuItem = bindProps(Menu.Item, props);
+
+function MainMenu(props) {
 	return (
 		<Menu>
-			<Menu.Item
-				as={NavLink}
-				to="/"
-				exact
-				activeClassName="active"
-				content="reviews"
-			/>
-			<Menu.Item
-				as={NavLink}
-				to="/submit"
-				exact
-				activeClassName="active"
-				content="submit new review"
-			/>
-			<Menu.Item
-				as={NavLink}
-				to="/delete"
-				exact
-				activeClassName="active"
-				content="delete reviews"
-			/>
+			<BoundMenuItem to="/" content="reviews" />
+			<BoundMenuItem to="/submit" content="submit new review" />
+			<BoundMenuItem to="/delete" content="delete reviews" />
 		</Menu>
 	);
-};
+}
 
 export default MainMenu;
