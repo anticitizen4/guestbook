fff9b8399851d36efaeee73b27b4e6dbf02b6b05
anticitizen4 <20382674+anticitizen4@users.noreply.github.com>
2018-11-09 08:03:42 +0300

    extract delete, make links & menu items work together

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from "react";
-import { Menu, Button } from "semantic-ui-react";
-import { Route, Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import { Route, NavLink } from "react-router-dom";
 
 import Reviews from "./Reviews";
 import Submit from "./Submit";
-import api from "./api";
+import Delete from "./Delete";
 
 import "./main.scss";
 
 class App extends Component {
-	handleDelete() {
-		api.deleteAll();
-	}
-
 	render() {
 		return (
 			<div className="container">
 				<Menu>
-					<Menu.Item>
-						<Link to="/">reviews</Link>
-					</Menu.Item>
-					<Menu.Item>
-						<Link to="/submit">submit new review</Link>
-					</Menu.Item>
-					<Button
-						onClick={this.handleDelete}
-						content="delete all reviews"
+					<Menu.Item
+						as={NavLink}
+						to="/"
+						exact
+						activeClassName="active"
+						content="reviews"
+					/>
+					<Menu.Item
+						as={NavLink}
+						to="/submit"
+						exact
+						activeClassName="active"
+						content="submit new review"
+					/>
+					<Menu.Item
+						as={NavLink}
+						to="/delete"
+						exact
+						activeClassName="active"
+						content="delete reviews"
 					/>
 				</Menu>
+
 				<Route exact path="/" component={Reviews} />
 				<Route exact path="/submit" component={Submit} />
+				<Route exact path="/delete" component={Delete} />
 			</div>
 		);
 	}
---
+++ client/src/Delete.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Segment, Button } from "semantic-ui-react";
+import api from "./api";
+
+class Delete extends Component {
+	handleDelete() {
+		api.deleteAll();
+	}
+
+	render() {
+		return (
+			<Segment>
+				<Button
+					onClick={this.handleDelete}
+					content="delete all reviews"
+					basic
+					compact
+				/>
+			</Segment>
+		);
+	}
+}
+
+export default Delete;
